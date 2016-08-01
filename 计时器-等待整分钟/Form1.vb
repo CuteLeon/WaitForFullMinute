deba30b206e106678f6149e993ebbf31cd6c4489
Leon <xiaoyankeji@hotmail.com>
2016-08-01 13:47:03 +0800

    第一次提交

---
+++ 计时器-等待整分钟/Form1.vb
@@ -0,0 +1,35 @@
+﻿Imports Microsoft.Win32
+Imports System.Threading
+
+Public Class Form1
+    Dim WaitThread As Thread = New Thread(AddressOf WaitM)
+    Dim WaitMillisecond As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        '监听用户改变系统时间
+        AddHandler SystemEvents.TimeChanged, AddressOf UserChangeTime
+
+        WaitThread.Start()
+    End Sub
+
+    Private Sub UserChangeTime(sender As Object, e As System.EventArgs)
+        Me.Text = "外部修改了系统时间"
+
+        If WaitThread.ThreadState = ThreadState.Running Then
+            WaitThread.Abort()
+            WaitThread = New Thread(AddressOf WaitM)
+        End If
+
+        WaitThread.Start()
+    End Sub
+
+    Private Sub WaitM()
+        Do While True
+            WaitMillisecond = 999 - Now.Millisecond + (59 - Now.Second) * 1000
+            Me.Text = ("距离下次整分钟还有：" & WaitMillisecond & " 毫秒")
+            Thread.Sleep(WaitMillisecond)
+            Me.Text = ("整分钟咯！  " & Now.Minute)
+        Loop
+    End Sub
+
+End Class
